63639195761d5540b32825b45e46390e936c8b1d
zybsw <841733577@qq.com>
2021-03-04 16:53:42 +0800

    vue项目入口

--- myblogvue/src/App.vue
+++ myblogvue/src/App.vue
@@ -1,18 +1,27 @@
 <template>
   <div id="app">
+    <Header />
+    <Main />
+    <!-- <FooterContent /> -->
   </div>
 </template>
 
 <script>
+import Header from "components/content/Header";
+import Main from "components/content/Main";
+import Footer from "components/content/Footer";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    
-  }
-}
+    Header,
+    Main,
+    Footer,
+  },
+};
 </script>
 
 <style>
-
+@import "assets/css/base.css";
+@import "assets/css/index.css";
 </style>
--- myblogvue/src/main.js
+++ myblogvue/src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router/index'
+
+import  './assets/icons/iconfont.css'
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: function (h) { return h(App) },
 }).$mount('#app')
