30a6cedb01fbaae2c83c1d39972aabe8fc28f9b9
zybsw <841733577@qq.com>
2021-03-04 16:51:13 +0800

    项目配置

---
+++ myblogvue/vue.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+function resolve(dir) {
+    return path.join(__dirname, dir)
+}
+
+
+module.exports = {
+    publicPath : './',
+    chainWebpack: (config) => {
+        config.resolve.alias
+            .set('@', resolve('./src'))
+            .set('components', resolve('./src/components'))
+            .set('assets', resolve('./src/assets'))
+            .set('views', resolve('./src/views'))
+            .set('network', resolve('./src/network'))
+    }
+}
