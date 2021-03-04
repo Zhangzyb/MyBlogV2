737cd5ea65ad514e40cb6b9962e89c6f8695b587
zybsw <841733577@qq.com>
2021-03-04 16:55:30 +0800

    Main区域组件

---
+++ myblogvue/src/components/content/MainContent.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="main-con">
+  <router-view></router-view>
+</div>
+</template>
+
+<script>
+export default {
+  name: "MainContent"
+  }
+</script>
+
+<style  scoped>
+.main-con{
+  flex: 3;
+  display: flex;
+  flex-flow: wrap;
+  align-content: space-between;
+  padding: 0 20px;
+}
+</style>
