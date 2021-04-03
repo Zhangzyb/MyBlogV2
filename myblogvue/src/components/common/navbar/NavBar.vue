258640b40bfd061db550443f17f89deee2010a38
zybsw <841733577@qq.com>
2021-04-03 10:01:05 +0800

    导航栏

--- myblogvue/src/components/common/navbar/NavBar.vue
+++ myblogvue/src/components/common/navbar/NavBar.vue
@@ -1,51 +1,72 @@
 <template>
-  <div class="nav-aside">
-    <div class="search">
-      <input type="text" placeholder="输入关键字" />
-      <i class="iconfont iconsousuo"></i>
+  <div class="nav-wrap">
+    <div class="nav-aside" :class="isShow ? 'show' : 'hide'">
+      <div
+        class="menu-btn"
+        :class="isShow ? 'open' : 'close'"
+        @click="menuClick"
+      >
+        <div class="line" :class="isShow ? 'top-line' : ''"></div>
+        <div class="line" :class="isShow ? 'mid-line' : ''"></div>
+        <div class="line" :class="isShow ? 'bot-line' : ''"></div>
+      </div>
+      <div class="search">
+        <input type="text" placeholder="输入关键字" v-model="query" />
+        <i class="iconfont iconsousuo" @click="searchClick"></i>
+      </div>
+      <div class="nav aside-con">
+        <div class="note">导航</div>
+        <nav-bar-item
+          v-for="(item, index) in nav"
+          :key="index"
+          :path="item.path"
+        >
+          <a href="javascript:;"
+            ><i class="iconfont" :class="item.class"></i>{{ item.name }}</a
+          >
+        </nav-bar-item>
+      </div>
+      <div class="tools aside-con">
+        <div class="note">工具</div>
+        <nav-bar-item v-for="(tool, index) in tools" :key="index">
+          <a :href="tool.link" target="blank"
+            ><i class="iconfont" :class="tool.class"></i>{{ tool.name }}</a
+          >
+        </nav-bar-item>
+      </div>
     </div>
-    <div class="nav aside-con">
-      <div class="note">导航</div>
-      <nav-bar-item v-for="(item, index) in nav" :key="index" :path="item.path">
-        <a href="javascript:;"><i class="iconfont" :class="item.class"></i>{{ item.name }}</a>
-      </nav-bar-item>
-    </div>
-    <div class="tools aside-con">
-      <div class="note">工具</div>
-      <nav-bar-item v-for="(tool, index) in tools" :key="index">
-        <a :href="tool.link" target="blank"><i class="iconfont" :class="tool.class"></i>{{ tool.name }}</a>
-      </nav-bar-item>
-    </div>
+    <div :class="isShow ? 'mask' : 'mask-hide'" @click="isShow = false"></div>
   </div>
 </template>
 
 <script>
-import NavBarItem from './NavBarItem'
+import NavBarItem from "components/common/navbar/NavBarItem";
 
 export default {
   name: "Aside",
   data() {
     return {
+      query: "",
       nav: [
         {
           class: "iconshouye",
           name: "首页",
-          path: '/'
+          path: "/",
         },
         {
           class: "iconfenlei",
           name: "分类",
-          path: '/category'
+          path: "/category",
         },
         {
           class: "iconshiguangzhou",
           name: "时光轴",
-          path: '/timeline'
+          path: "/timeline",
         },
         {
           class: "iconpengyouquan",
           name: "个人动态",
-          path: '/dynamic'
+          path: "/dynamic",
         },
       ],
       tools: [
@@ -70,62 +91,200 @@ export default {
           link: "https://www.iconfont.cn/",
         },
       ],
+      isShow: false,
     };
   },
   methods: {
-    navClick() {
-      this.$router.replace("/category");
+    menuClick() {
+      this.isShow = !this.isShow;
+    },
+    searchClick() {
+      if (!this.query) {
+        this.$swal.fire({
+          text: "请输入搜索内容",
+          icon: "warning",
+          showConfirmButton: false,
+          timer: 1000,
+        });
+      } else {
+        this.$router.push({
+          path: "/search/",
+          query: {
+            text: this.query,
+          },
+        });
+      }
     },
   },
   components: {
-    NavBarItem
-  }
+    NavBarItem,
+  },
 };
 </script>
 
 <style scoped>
-.nav-aside {
-  flex: 1;
+@media screen and (max-width: 769px) {
+  .nav-aside {
+    position: fixed;
+    width: 55vw;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    padding: 6rem 0.8rem 2rem 0.8rem;
+    background: rgba(0, 0, 0, 0.801);
+    transition: all 0.2s;
+    z-index: 99;
+  }
+  .show {
+    transform: translateX(0);
+  }
+
+  .hide {
+    transform: translateX(-105%);
+  }
+
+  .menu-btn {
+    position: absolute;
+    top: 0.4rem;
+    right: 1rem;
+    width: 2.5rem;
+    height: 36px;
+    z-index: 999;
+    cursor: pointer;
+  }
+  .menu-btn .line {
+    position: absolute;
+    width: 80%;
+    height: 3px;
+    background: rgb(255, 255, 255);
+    margin: 0.8rem 0;
+    transition: all 0.8s;
+  }
+  .top-line {
+    top: 10px;
+    transform: rotateZ(45deg);
+  }
+  .line:nth-child(2) {
+    top: 10px;
+  }
+
+  .mid-line,
+  .mask-hide {
+    display: none;
+  }
+
+  .line:nth-child(3) {
+    top: 20px;
+  }
+
+  .menu-btn .bot-line {
+    top: 10px;
+    transform: rotateZ(-45deg);
+  }
+
+  .close {
+    transform: translateX(240%);
+  }
+  .open {
+    transform: translateX(0);
+  }
+
+  .note {
+    color: rgba(253, 253, 253, 0.767);
+    font-size: 1.5rem;
+  }
+  .search {
+    padding: 0 0.2rem;
+  }
+
+  .search input {
+    height: 2.2rem;
+    margin-bottom: 1rem;
+    font-size: 1.2rem;
+    border-radius: 1rem;
+  }
+
+  .search i {
+    top: 0;
+    right: 1.2rem;
+    height: 2.2rem;
+    line-height: 2.2rem;
+    font-size: 1.5rem;
+  }
+  .mask {
+    position: fixed;
+    width: 100vw;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    background: rgba(0, 0, 0, 0.3);
+    z-index: 88;
+  }
+}
+
+@media screen and (min-width: 770px) {
+  .nav-wrap {
+    flex: 1;
+  }
+  .nav-aside {
+    position: relative;
+    padding: 0 2rem 0 0;
+  }
+
+  .aside-con {
+    border: 1px solid #e2e8ea;
+  }
+
+  .note {
+    color: #6d7c83;
+    font-size: 0.7rem;
+  }
+  .search {
+    margin: 0 0 0.3rem 0;
+    border: 1px solid #e2e8ea;
+    border-radius: 0.5rem;
+  }
+
+  .search input {
+    height: 1.5rem;
+    font-size: 0.8rem;
+    border-radius: 0.5rem;
+  }
+  .search i {
+    top: 0.1rem;
+    right: 0.5rem;
+    height: 2rem;
+    font-size: 1.1rem;
+  }
 }
 
 .aside-con {
-  border: 1px solid #e2e8ea;
   padding-bottom: 0.6rem;
 }
 
 .note {
   padding: 0.5rem;
-  color: #6d7c83;
-  font-size: 0.7rem;
 }
 
 .search {
   position: relative;
   width: 100%;
   margin: 0 0 0.3rem 0;
-  border: 1px solid #e2e8ea;
   border-radius: 0.5rem;
 }
 
 .search input {
   width: 100%;
-  height: 1.5rem;
-  padding-left: 0.8rem;
   border: none;
   outline: none;
-  font-size: 0.8rem;
+  padding-left: 0.8rem;
   color: #1f2224cc;
-  border-radius: 0.5rem;
 }
 
 .search i {
   position: absolute;
   display: block;
-  top: 0.1rem;
-  right: 0.5rem;
-  height: 2rem;
   color: #212121a1;
-  font-size: 1.1rem;
 }
-
 </style>
--- myblogvue/src/components/common/navbar/NavBarItem.vue
+++ myblogvue/src/components/common/navbar/NavBarItem.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="aside-item" @click="navClick" :class="isActive ? 'active':'' ">
+  <div class="aside-item" @click="navClick" :class="isActive ? 'active' : ''">
     <slot></slot>
   </div>
 </template>
@@ -9,8 +9,8 @@ export default {
   name: "NavBarItem",
   computed: {
     isActive() {
-      return this.$route.path == this.path
-    }
+      return this.$route.path == this.path;
+    },
   },
   props: {
     path: String,
@@ -26,32 +26,52 @@ export default {
 </script>
 
 <style scoped>
+@media screen and (max-width: 769px) {
+  .aside-item {
+    font-size: 1.3rem;
+    padding: 1.8rem 0.6rem; 
+  }
+  .aside-item a {
+    color: #fff;
+    text-shadow: 0 0 10px rgb(255 255 255 / 50%);
+  }
+  .aside-item a i {
+    margin-right: 1.6rem;
+    font-size: 1.6rem;
+  }
+}
+
+@media screen and (min-width: 770px) {
+  .aside-item {
+    font-size: 0.8rem;
+  }
+  .aside-item a {
+    color: #212121a1;
+  }
+  .aside-item:hover {
+    background-color: rgba(0, 0, 0, 0.05);
+  }
+  .active {
+    background: rgba(0, 0, 0, 0.05);
+  }
+  .aside-item a i {
+    margin-right: 1rem;
+    font-size: 1rem;
+  }
+}
+
 .aside-item {
   display: flex;
   height: 2rem;
   align-items: center;
   justify-content: center;
   width: 100%;
-  font-size: 0.8rem;
   transition: all 0.3s;
   cursor: pointer;
-}
-.aside-item:hover {
-  background-color: rgba(0, 0, 0, 0.05);
 }
 
 .aside-item a {
   width: 100%;
   padding-left: 1.2rem;
-  color: #212121a1;
-}
-
-.aside-item a i {
-  margin-right: 1rem;
-  font-size: 1rem;
-}
-
-.active{
-  background: rgba(0, 0, 0, 0.05);
 }
 </style>
