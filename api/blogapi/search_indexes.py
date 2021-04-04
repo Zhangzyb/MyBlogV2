2a93f8ae83bb0bea36b5f108632d72179fc071fe
zybsw <841733577@qq.com>
2021-04-04 12:33:13 +0800

    搜索引擎配置

---
+++ api/blogapi/search_indexes.py
@@ -0,0 +1,13 @@
+from haystack import indexes
+from .models import Article
+
+
+class ArticleIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True, use_template=True)
+
+    def get_model(self):
+        return Article
+
+    def index_queryset(self, using=None):
+        """Used when the entire index for model is updated."""
+        return self.get_model().objects.all()
