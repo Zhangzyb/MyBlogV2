d50399a7c418e75e0d8ce0075e32721bc1a91c1a
zybsw <841733577@qq.com>
2021-04-04 12:35:17 +0800

    数据库表的设计及配置

--- api/blogapi/admin.py
+++ api/blogapi/admin.py
@@ -1,23 +1,39 @@
 from django.contrib import admin
-from .models import Article, Tag, Category
+from .models import Article, ArticleDetail, Tag, Category, Comment
+
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['title', 'create_time', 'modified_time', 'category', 'author']
-    fields = ['title', 'image', 'text', 'category', 'tag']
+    list_display = ['id', 'title', 'abstract',
+                    'created_time', 'modified_time', 'category', 'author']
+    fields = ['title', 'image', 'category', 'tag']
 
     def save_model(self, request, obj, form, change):
         obj.author = request.user
         super().save_model(request, obj, form, change)
 
 
+class DetailAdmin(admin.ModelAdmin):
+    list_display = ['id', 'article', 'text']
+
+
 class TagAdmin(admin.ModelAdmin):
     list_display = ['name']
     fields = ['name']
 
+
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['name','parent']
-    fields = ['name','parent']
+    list_display = ['name']
+    fields = ['name']
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['username', 'text', 'id', 'email', 'article',
+                    'created_time', 'parent_comment']
+    fields = ['username', 'email', 'text', 'article', 'status','parent_comment','images']
+
 
 admin.site.register(Article, PostAdmin)
-admin.site.register(Tag,TagAdmin)
-admin.site.register(Category,CategoryAdmin)
+admin.site.register(ArticleDetail, DetailAdmin)
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Comment, CommentAdmin)
--- api/blogapi/models.py
+++ api/blogapi/models.py
@@ -1,15 +1,13 @@
 from django.db import models
-import markdown
 from django.contrib.auth.models import User
 from django.utils import timezone
 from django.utils.html import strip_tags
 from mdeditor.fields import MDTextField
+import markdown
 
 
 class Category(models.Model):
     name = models.CharField('分类', max_length=50)
-    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True,
-                               blank=True, related_name='subdir', verbose_name='上级目录')
 
     class Meta:
         db_table = 'category'
@@ -34,9 +32,8 @@ class Tag(models.Model):
 
 class Article(models.Model):
     title = models.CharField('标题', max_length=200)
-    text = MDTextField('正文')
     image = models.CharField('图片', max_length=500)
-    create_time = models.DateTimeField('创建时间', default=timezone.now)
+    created_time = models.DateTimeField('创建时间', default=timezone.now)
     modified_time = models.DateTimeField('修改时间')
     author = models.ForeignKey(
         User, verbose_name='作者', on_delete=models.CASCADE)
@@ -57,17 +54,57 @@ class Article(models.Model):
 
     def save(self, *args, **kwargs):
         self.modified_time = timezone.now()
+        super().save(*args, **kwargs)
+
+
+class ArticleDetail(models.Model):
+    article = models.OneToOneField(
+        Article, on_delete=models.CASCADE, verbose_name='文章')
+    text = MDTextField('正文')
+
+    class Meta:
+        db_table = 'article_detail'
+        verbose_name = '文章详情'
+        verbose_name_plural = verbose_name
+
+    def save(self, *args, **kwargs):
+        self.article.modified_time = timezone.now()
         md = markdown.Markdown(extensions=[
             'markdown.extensions.extra',
             'markdown.extensions.codehilite',
         ])
-        self.abstract = strip_tags(md.convert(self.text))[:200]
+
+        self.article.abstract = strip_tags(md.convert(self.text))[:200]
+        self.article.save()
         super().save(*args, **kwargs)
 
-    def inc_views(self):
-        self.views += 1
-        self.save(update_fields=['views'])
 
-    def inc_likes(self):
-        self.likes += 1
-        self.save(update_fields=['likes'])
+class Comment(models.Model):
+    username = models.CharField('用户名', max_length=30)
+    avatar = models.CharField('头像', max_length=100)
+    email = models.EmailField('邮箱')
+    text = models.TextField('评论内容')
+    created_time = models.DateTimeField('评论时间', default=timezone.now)
+    article = models.ForeignKey(
+        'Article', verbose_name='所属文章', on_delete=models.CASCADE)
+    parent_comment = models.ForeignKey(
+        'self', on_delete=models.CASCADE, null=True, blank=True, verbose_name='父级评论')
+    reply_name = models.CharField('回复名称', max_length=100, null=True)
+    agree = models.IntegerField('赞成数', default=0)
+    disagree = models.IntegerField('反对数', default=0)
+    status = models.IntegerField('类别', default=0)
+    images = models.CharField('动态图片', max_length=150,
+                              null=True, blank=True, default=None)
+
+    class Meta:
+        ordering = ['-created_time']
+        verbose_name = '评论'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.username
+
+    def save(self, *args, **kwargs):
+        qq = self.email.split('@')[0]
+        self.avatar = f'https://q.qlogo.cn/headimg_dl?dst_uin={qq}&spec=100'
+        super().save(*args, **kwargs)
