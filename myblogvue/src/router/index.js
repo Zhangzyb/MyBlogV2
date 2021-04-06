4dfe254088df043bc1895dc8adb95be2e7fd8ffa
zybsw <841733577@qq.com>
2021-04-06 11:47:10 +0800

    vue router配置

--- myblogvue/src/router/index.js
+++ myblogvue/src/router/index.js
@@ -5,6 +5,8 @@ const Home = () => import('views/home/Home')
 const Category = () => import('views/category/Category')
 const TimeLine = () => import('views/timeline/TimeLine')
 const Dynamic = () => import('views/dynamic/Dynamic')
+const Detail = () => import('views/detail/Detail')
+const Search = () => import('views/search/Search')
 
 Vue.use(VueRouter)
 
@@ -24,12 +26,20 @@ const routes = [
     {
         path: '/dynamic',
         component: Dynamic
+    },
+    {
+        path: '/post/:id',
+        component: Detail
+    },
+    {
+        path:'/search/',
+        component: Search
     }
 ]
 
 const router = new VueRouter({
     routes,
-    mode: 'history'
+    // mode: 'history'
 })
 
 export default router
